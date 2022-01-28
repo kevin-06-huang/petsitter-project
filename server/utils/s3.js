a33a6000ee16c59888018eb9f5922dfba166f0e9
Kevin <kevin.06.huang@gmail.com>
2022-01-27 16:12:03 -0800

    Fixed dependency error

--- server/utils/s3.js
+++ server/utils/s3.js
@@ -1,4 +1,4 @@
-const s3 = require('aws-sdk/clients/s3')
+const S3 = require('aws-sdk/clients/s3')
 const bucketName = process.env.AWS_BUCKET_NAME
 const region = process.env.AWS_BUCKET_REGION
 const accessKeyId = process.env.AWS_ACCESS_KEY
@@ -10,7 +10,7 @@ const s3 = new S3({
     secretAccessKey
 });
 
-export function uploadFile(file) {
+const uploadFile = (file) => {
     const fileStream = fs.createReadStream(file.path)
 
     const uploadParams = {
@@ -22,4 +22,4 @@ export function uploadFile(file) {
     return s3.upload(uploadParams).promise()
 };
 
-exports.uploadFile = uploadFile;
+module.exports = uploadFile;
