3ba9d9847cb080c13da6c123b745e37937ee55ca
JunZheng007 <jzheng0721@gmail.com>
2022-01-28 13:05:06 -0500

    add loading spinner for the split second

--- client/src/components/AuthRoute/AuthRoute.tsx
+++ client/src/components/AuthRoute/AuthRoute.tsx
@@ -1,7 +1,15 @@
 import { Redirect, Route, RouteProps } from 'react-router-dom';
 import { useAuth } from '../../context/useAuthContext';
+import { CircularProgress } from '@mui/material';
+import React from 'react';
 
 export const AuthRoute = (props: RouteProps) => {
   const { loggedInUser } = useAuth();
-  return loggedInUser ? <Route {...props} /> : <Redirect to="/login" />;
+  return loggedInUser === undefined ? (
+    <CircularProgress />
+  ) : loggedInUser ? (
+    <Route {...props} />
+  ) : (
+    <Redirect to="/login" />
+  );
 };
