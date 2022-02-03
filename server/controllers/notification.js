9e95f34483aee5cf30fb001c9435d3dd09b0b450
Kevin <kevin.06.huang@gmail.com>
2022-02-03 09:18:20 -0800

    implement notification controller skeleton

---
+++ server/controllers/notification.js
@@ -0,0 +1,18 @@
+const asyncHandler = require("express-async-handler");
+//createNotification, readNotification, getAll, getUnread
+// @route POST /image/upload
+// @desc Upload image(s)
+// @access Private
+exports.createNotification = asyncHandler(async (req, res) => {
+    res.status(200);
+});
+
+exports.readNotification = asyncHandler(async (req, res) => {
+    res.status(200);
+});
+exports.getAll = asyncHandler(async (req, res) => {
+    res.status(200);
+});
+exports.getUnread = asyncHandler(async (req, res) => {
+    res.status(200);
+});
