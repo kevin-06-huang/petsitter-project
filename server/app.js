be488919f67ea64893e27c2c62d9af3d6b8e7e57
Amita <panwar.amita@gmail.com>
2022-02-04 12:57:51 -0500

    added the calender icon

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ const logger = require("morgan");
 const authRouter = require("./routes/auth");
 const userRouter = require("./routes/user");
 const profileRouter = require('./routes/profile');
-// const imageRouter = require('./routes/image');
+const imageRouter = require('./routes/image');
 
 const { json, urlencoded } = express;
 
@@ -46,7 +46,7 @@ app.use((req, res, next) => {
 app.use("/auth", authRouter);
 app.use("/users", userRouter);
 app.use("/profile", profileRouter);
-// app.use("/image", imageRouter);
+app.use("/image", imageRouter);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/client/build")));
