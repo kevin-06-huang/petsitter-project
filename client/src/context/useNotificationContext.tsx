c707a08249dd36eb618ae6a189d6098308f3a856
Kevin <kevin.06.huang@gmail.com>
2022-02-06 07:51:04 -0800

    Fixed update error, implement useeffect for notificationcontext, added snackbar

--- client/src/context/useNotificationContext.tsx
+++ client/src/context/useNotificationContext.tsx
@@ -2,7 +2,8 @@ import { useState, useContext, createContext, FunctionComponent, SyntheticEvent,
 import { Notification } from '../interface/Notification';
 import { useAuth } from './useAuthContext';
 import getNotifications from '../helpers/APICalls/getNofitications';
-import { NotificationApiData, NotificationApiDataGetAll } from '../interface/NotificationApiData';
+import { NotificationApiData } from '../interface/NotificationApiData';
+import { useSnackBar } from './useSnackbarContext';
 
 interface INotificationContext {
   notifications: [Notification] | undefined;
@@ -17,25 +18,29 @@ export const NotificationContext = createContext<INotificationContext>({
 export const NotificationContextProvider: FunctionComponent = ({ children }): JSX.Element => {
   const [notifications, setNotifications] = useState<[Notification] | undefined>(undefined);
   const { profile } = useAuth();
+  const { updateSnackBarMessage } = useSnackBar();
 
-  const pushNotification = useCallback((notification: Notification) => {
-    notifications!.push(notification);
-    //api call to database update
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  const pushNotification = useCallback(
+    (notification: Notification) => {
+      notifications!.push(notification);
+      //api call to database update
+    },
+    [notifications],
+  );
 
   useEffect(() => {
     const updateNotifications = async (userId: string) => {
       await getNotifications(userId).then((data: NotificationApiData) => {
         if (data.success) {
           setNotifications(data.success.notifications);
+          updateSnackBarMessage('You have new notifications!');
         } else {
-          // can use snackbar here?
-          console.log(data.error!.message);
+          updateSnackBarMessage(data.error!.message);
         }
       });
     };
     profile && updateNotifications(profile.userId);
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  }, [profile, updateSnackBarMessage]);
 
   return (
     <NotificationContext.Provider value={{ notifications, pushNotification }}>{children}</NotificationContext.Provider>
