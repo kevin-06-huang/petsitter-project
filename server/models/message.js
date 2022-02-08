cc71d6a7c86bd05a67cb593cbd1ec1db555809bd
Amita <panwar.amita@gmail.com>
2022-02-07 19:07:36 -0500

    created message model

---
+++ server/models/message.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const messageSchema = new mongoose.Schema({
+    text: {
+        type: String,
+        required: true
+    },
+    senderId: {
+        type: mongoose.Types.ObjectId,
+        ref: 'Profile',
+        required: true
+    },
+    conversationId: {
+        type: Types.ObjectId,
+        ref: 'Conversation',
+        required: true
+    },
+
+},
+    {
+        timestamps: true
+    });
+
+const Message=mongoose.model('Message',messageSchema);
+
+module.exports=Message;
