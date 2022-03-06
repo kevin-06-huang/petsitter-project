1d2d4d9559f8b1f080a0f9952735f6f71579a227
Kevin <kevin.06.huang@gmail.com>
2022-03-05 16:00:20 -0800

    Merge

--- client/src/pages/Dashboard/Dashboard.tsx
+++ client/src/pages/Dashboard/Dashboard.tsx
@@ -1,16 +1,22 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAuth } from '../../context/useAuthContext';
 import { useSocket } from '../../context/useSocketContext';
 import { useHistory } from 'react-router-dom';
-import { CircularProgress, Grid, Typography } from '@mui/material';
+import { CircularProgress, Grid, Typography, Box, Button } from '@mui/material';
 import PageContainer from '../../components/PageContainer/PageContainer';
+import SearchBar from '../ProfileList/searchBar';
+import ProfileCard from '../ProfileList/profileCard/ProfileCard';
+import useStyle from './useStyles';
 
 export default function Dashboard(): JSX.Element {
   const { loggedInUser } = useAuth();
   const { initSocket } = useSocket();
   const history = useHistory();
+  const classes = useStyle();
+  //const [profiles, setProfiles] = useState<[Profile] | undefined>(undefined);
 
   useEffect(() => {
+    console.log('test');
     initSocket();
   }, [initSocket]);
 
@@ -29,6 +35,35 @@ export default function Dashboard(): JSX.Element {
             Search Profiles
           </Typography>
         </Grid>
+        <Box className={classes.title}> </Box>
+        <SearchBar />
+        <Grid
+          className={classes.box}
+          container
+          spacing={3}
+          rowSpacing={1}
+          columnSpacing={{ xs: 2, sm: 5, md: 5 }}
+          alignItems="center"
+          justifyContent="center"
+        >
+          {[1, 2, 3, 4, 5, 6].map((card) => (
+            <Box key={card} className={classes.card}>
+              <ProfileCard name={`${card}`} />
+            </Box>
+          ))}
+        </Grid>
+        <Box textAlign="center" marginTop={5} className={classes.showMore}>
+          <Button
+            sx={{ background: 'none', color: 'black', border: '1px solid #dbdbdb' }}
+            type="submit"
+            size="large"
+            variant="contained"
+            className={classes.submit}
+            disableElevation
+          >
+            show more
+          </Button>
+        </Box>
       </Grid>
     </PageContainer>
   );
